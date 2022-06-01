db0ce5d51559ee3f5cbaed71af5db71d51d3178b
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 18:55:42 +0100

    Created the query for adding a student

--- src/student/queries.js
+++ src/student/queries.js
@@ -2,7 +2,8 @@ const getStudents = "SELECT * FROM students";
 
 const getStudentById = "SELECT * FROM students WHERE ID = $1";
 
-const addStudent = "INSERT INTO students (name, age";
+const addStudent = "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
+
 module.exports = {
     getStudents, 
     getStudentById, 
