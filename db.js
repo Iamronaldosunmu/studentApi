fe575d72fb512f18f83ad914c4e0ab87ff320bd8
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 18:52:03 +0100

    Fixed configuration errors in the db file

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
-const Pool = new Pool({
+const pool = new Pool({
     user: "postgres", 
     host: "localhost", 
     database: "students", 
     password: "Movenpick123", 
-    port: 5432
+    port: 5433
 } 
 );
-export default Pool
+module.exports = pool;
