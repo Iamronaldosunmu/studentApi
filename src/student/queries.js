7af9a232d8d4299df8f35fd5bfdb04b5ba227344
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 18:56:44 +0100

    Created the query for deleting a student

--- src/student/queries.js
+++ src/student/queries.js
@@ -4,11 +4,14 @@ const getStudentById = "SELECT * FROM students WHERE ID = $1";
 
 const addStudent = "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
 
-const checkEmailExists = "SELECT * FROM students WHERE email = $1";
+const checkEmailExists = "SELECT s FROM students s WHERE email = $1";
+
+const removeStudent = "DELETE FROM students WHERE ID = $1";
 
 module.exports = {
     getStudents, 
     getStudentById, 
     addStudent, 
-    checkEmailExists
+    checkEmailExists, 
+    removeStudent
 };
