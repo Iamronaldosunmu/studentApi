2cbe85cf524f959dcc70c2ef4b515e267e7e9176
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 18:56:11 +0100

    Created a query to check if the email exists

--- src/student/queries.js
+++ src/student/queries.js
@@ -4,8 +4,11 @@ const getStudentById = "SELECT * FROM students WHERE ID = $1";
 
 const addStudent = "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
 
+const checkEmailExists = "SELECT * FROM students WHERE email = $1";
+
 module.exports = {
     getStudents, 
     getStudentById, 
-    addStudent
+    addStudent, 
+    checkEmailExists
 };
