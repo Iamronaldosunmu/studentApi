d5444d0596f6e8543bbbebab6c1e6f028b4afd13
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 18:52:34 +0100

    Added the json middleware to ensure the api can parse json requests

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 3000;
 const studentRoutes = require('./src/student/routes');
  
+app.use(express.json());
 
 app.use('/api/v1/students', studentRoutes);
 app.get('/', (req, res) => {
-    res.send('The server is running')
+    res.send('The server is running');
 });
 
 app.listen(port, () => console.log(`The server is now running on port ${port}`));
