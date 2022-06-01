9b568511eee2f93819eb3fb9beaa78ae9e467456
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 18:53:45 +0100

    Added the delete route to the routes.js file

--- src/student/routes.js
+++ src/student/routes.js
@@ -4,5 +4,6 @@ const router = Router();
 
 router.get('/', controller.getStudents); 
 router.get('/:id', controller.getStudentById);
-router.post('/', controller.addStudent)
+router.post('/', controller.addStudent);
+router.delete('/:id', controller.deleteStudent);
 module.exports = router;
