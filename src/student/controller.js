dc8babc3d152befe2ab1f8693e9d54667699d0e8
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 19:12:02 +0100

    Created the controller for removing a student

--- src/student/controller.js
+++ src/student/controller.js
@@ -22,19 +22,36 @@ const addStudent = (req, res) => {
     // Check if email exists
     pool.query(queries.checkEmailExists, [email], (error, results) => {
         if (results.rows.length) {
-            res.send("Email already exists. ");
+            return res.send("Email already exists. ");
         }
     })
 
     // add student to the db
     pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
         if (error) throw error;
-        res.status(201).send("Student Created Successfully!");
+        return res.status(201).send("Student Created Successfully!");
     })
   
+}
+
+const removeStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query(queries.removeStudent, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound){
+            return res.send("Student does not exist in the database");
+        }
+    })
+
+    pool.query(queries.removeStudent, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send("Student removed successfully.");
+    })
 }
 module.exports = {
     getStudents,
     getStudentById, 
-    addStudent
+    addStudent, 
+    removeStudent
 }
