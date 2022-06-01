156aea0949fef1ca2e9bb794c8321c2e59651aee
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 17:50:43 +0100

    Added the query to add a student

--- src/student/routes.js
+++ src/student/routes.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
+const controller = require('./controller');
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send("Using api route");
-})
-
+router.get('/', controller.getStudents); 
+router.get('/:id', controller.getStudentById);
+router.post('/', controller.addStudent)
 module.exports = router;
