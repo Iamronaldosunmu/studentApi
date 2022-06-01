98362b93c3b91317cabcbe0e513e83238e4a6634
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 19:05:57 +0100

    Created the controller to add a student

--- src/student/controller.js
+++ src/student/controller.js
@@ -4,20 +4,37 @@ const queries = require('./queries');
 const getStudents = (req, res) => {
     pool.query(queries.getStudents, (error, results) => {
         if (error) throw error;
-        res.status(200).json(results.rows);
+        return res.status(200).json(results.rows);
 
     });
 
 } 
 const getStudentById = (req, res) => {
     const id = parseInt(req.params.id);
-    console.log(id)
     pool.query(queries.getStudentById, [id], (error, results) => {
-        // if (error) throw error;
-        res.send(200).json(results.rows);
+        if (error) throw error;
+        return res.send(200).json(results.rows);
     })
+}
+
+const addStudent = (req, res) => {
+    const { name, email, age, dob } = req.body;
+    // Check if email exists
+    pool.query(queries.checkEmailExists, [email], (error, results) => {
+        if (results.rows.length) {
+            res.send("Email already exists. ");
+        }
+    })
+
+    // add student to the db
+    pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
+        if (error) throw error;
+        res.status(201).send("Student Created Successfully!");
+    })
+  
 }
 module.exports = {
     getStudents,
-    getStudentById
+    getStudentById, 
+    addStudent
 }
