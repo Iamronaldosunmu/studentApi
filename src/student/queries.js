633a768c0659e1a18b7567fed5d3252287da372e
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 18:57:16 +0100

    Created a query to update the name of a student

--- src/student/queries.js
+++ src/student/queries.js
@@ -8,10 +8,12 @@ const checkEmailExists = "SELECT s FROM students s WHERE email = $1";
 
 const removeStudent = "DELETE FROM students WHERE ID = $1";
 
+const updateStudent = "UPDATE students SET name = $1 WHERE ID = $2 "
 module.exports = {
     getStudents, 
     getStudentById, 
     addStudent, 
     checkEmailExists, 
-    removeStudent
+    removeStudent, 
+    updateStudent
 };
